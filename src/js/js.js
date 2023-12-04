a161c25c9a1566ac2f9f0785d64d984f96ac213a
vaibhavlvl1 <acevaibhav14@gmail.com>
2023-12-04 10:57:53 +0530

    final

---
+++ src/js/js.js
@@ -0,0 +1,11 @@
+// glight box
+
+const videobox = GLightbox({
+    autoplayVideos: true,
+    loop: true,
+    touchNavigation: true,
+    'href': 'https://youtu.be/aau-c8l5z9c',
+    'type': 'video',
+    'source': 'youtube',
+    'width': 900,
+})
